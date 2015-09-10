5a37a3e38b15dd5decb6edd269b80e2e891eec99
jantsankhorloo <jantsankhorloo@gmail.com>
2015-09-09 23:40:20 -0400

    deleted unnecassary files

--- Object Oriented Programming/inheritance.py
+++
@@ -1,8 +0,0 @@
-class Parent():
-    def __init__(self, last_name, eye_color):
-        print ('Parent Constructor Called')
-        self.last_name = last_name
-        self.eye_color = eye_color
-
-billy_cyrus = Parent('Cyrus','Blue')
-print (billy_cyrus.last_name)
